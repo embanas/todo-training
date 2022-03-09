37e01ffe2a77e104677a621538e9c74b99506b23
embanas <you@example.com>
2022-03-09 11:19:51 +0100

    employee details commit

---
+++ src/app/pages/employee-details.page.ts
@@ -0,0 +1,17 @@
+import {
+  Component,
+  ViewEncapsulation,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'lib-employee-details-page',
+  templateUrl: './employee-details.page.html',
+  encapsulation: ViewEncapsulation.None,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class EmployeeDetailsPage {
+    params$ = this._activatedRoute.params;
+    constructor(private _activatedRoute: ActivatedRoute) {}
+}
